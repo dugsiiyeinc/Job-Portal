6a610320890f74f526fc3dfe1474b8d93e55abbb
ayale3 <ayanle876@gmail.com>
2024-10-25 22:32:46 +0300

    add: funtion to show job post to the dom

--- js/jobs.js
+++ js/jobs.js
@@ -1,7 +1,26 @@
 const  jobList  = document.querySelector('#jobList')
 
-window.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded",loadJobsdata);
 
+function loadJobsdata() {
+    const jobs = getPostFromLocalStorage();
+
+     console.log(jobs)
+    //  function to display to dom
+    addJobsToTheDom(jobs)
+
+
+}
+// get data from local storage
+function getPostFromLocalStorage(){ 
+    const oldJobPostDetail = JSON.parse(localStorage.getItem('jobPosts')) || [];
+     
+    return oldJobPostDetail
     
+}
+ 
+// kun soo  bandhiga jobs  dom-ka
 
-})
+function addJobsToTheDom (jobPosts){
+    
+}
