f1b5eaf3cfa7842df7f1c39ecaf05851ae3537d4
shiinedev <shiinecadde17@gmail.com>
2024-10-30 11:02:50 +0300

    showing user job detials in js

--- js/jobdetails.js
+++ js/jobdetails.js
@@ -43,21 +43,66 @@ function getJobToShow(id) {
 
 // kun soo  bandhiga jobs  dom-ka
 
-// function addJobsToTheDom(jobPosts) {
-//     console.log(jobPosts)
-//     const formatedJobData = jobPosts.postAreatext.replace(/\n/g, '<br>')
-//     const jobCard = document.querySelector('.content');
+function addJobsToTheDom(jobPosts) {
+    console.log(jobPosts)
+    const formatedJobData = jobPosts.postAreatext.replace(/\n/g, '<br>')
+    const jobCard = document.querySelector('#job-details');
 
-//     jobCard.innerHTML = `
-//                  <h1>${jobPosts.postTitle}</h1>
-//                   <h3>${jobPosts.companyInput}</h3>
-//                  <p class="secondary-color">  Expire date :  ${jobPosts.dateInput}  </p>
-//                  <p > ${formatedJobData} </p>
+    jobCard.innerHTML = `
+                 <div class="content all-job-details">
+        <h4 class="details-title">job details</h4>
+        <div class="jobs-details">
+       
+        <div class="job-deatil">
+         
+          <div class="job-deatail-info posted-date">
+            <i class="fa-solid fa-calendar-days"></i>
+            <div class="job-deatail-text">
+              <h4>${jobPosts.dateInput}</h4>
+              <p>Posted Date</p>
+            </div>
+          </div>
+        </div>
+        <div class="job-deatail">
+          <div class="job-deatail-info posted-date">
+            <i class="fa-regular fa-calendar"></i>
+            <div class="job-deatail-text">
+              <h4>${jobPosts.dateInput}</h4>
+              <p>Expire Date</p>
+            </div>
+          </div>
+        </div>
+        <div class="job-deatail">
+          <div class="job-deatail-info posted-date">
+            <i class="fa-solid fa-layer-group"></i>
+            <div class="job-deatail-text">
+              <h4>${jobPosts.jobCategory}</h4>
+              <p>Category</p>
+            </div>
+          </div>
+        </div>
+        <div class="job-deatail">
+          <div class="job-deatail-info posted-date">
+            <i class="fa-solid fa-location-dot"></i>
+            <div class="job-deatail-text">
+              <h4>${jobPosts.postLocation}</h4>
+              <p>Location</p>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+      </div>
+      <div class="job-descreption">
+        <h4 class="desc-title">job Description</h4>
+        <p class="desc-text">${formatedJobData}</p>
+      </div>
+    </div>
                  
-//                 `;
+                `;
 
-//     document.title = `${jobPosts.postTitle}`
+    document.title = `${jobPosts.postTitle}`
 
 
-// }
+}
 
