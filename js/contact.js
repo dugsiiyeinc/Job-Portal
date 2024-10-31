8c1021f251a20f900bd3f6e61b7cc4bb13fea5ca
shiinedev <shiinecadde17@gmail.com>
2024-10-31 14:01:05 +0300

    set log in buttons when loptop or mobile menu

--- js/contact.js
+++ js/contact.js
@@ -3,3 +3,11 @@ const onlineUserName = document.querySelector(".username");
 const loginBtn1 = document.querySelectorAll(".loginBtn")[0];
 const loginBtn2 = document.querySelectorAll(".loginBtn")[1];
 const dashLink = document.querySelector('.dash-link');
+
+//set log in buttons when loptop or mobile menu
+loginBtn1.addEventListener("click", () => {
+    localStorage.removeItem("onlineUser")
+});
+loginBtn2.addEventListener("click", () => {
+    localStorage.removeItem("onlineUser")
+});
