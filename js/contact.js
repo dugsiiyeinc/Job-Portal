fd16efbd4da0a6b25c054d5588df24d5e327a01e
shiinedev <shiinecadde17@gmail.com>
2024-10-31 14:00:15 +0300

    select dom elements in contact.js

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+const showUserInfo = document.querySelector("#user");
+const onlineUserName = document.querySelector(".username");
+const loginBtn1 = document.querySelectorAll(".loginBtn")[0];
+const loginBtn2 = document.querySelectorAll(".loginBtn")[1];
+const dashLink = document.querySelector('.dash-link');
